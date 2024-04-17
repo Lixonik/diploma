8551067ef1581767f57018c3c2b70d6f1a4c3cd9
Boris Prokhorov <deimos.75@mail.ru>
2024-04-17 21:35:24 +0300

    update lib version

--- index.ts
+++ index.ts
@@ -1,6 +1,18 @@
-import { generateUUID } from 'test-data-utils'
+import { generateMeaningfulString, generateNumber, generatePerson, generateString, generateUUID } from 'test-data-utils'
 
 
 for (let i = 0; i < 4097; i++) {
-    console.log(generateUUID());
+	console.log(generateUUID())
 }
+
+console.log(generateMeaningfulString({
+	length: 1000,
+	separator: ' ',
+}))
+
+console.log(generatePerson())
+
+
+console.log(generateNumber())
+
+console.log(generateString())
