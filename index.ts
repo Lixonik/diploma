24f05b785056a5f9faf2eef595625090feb8f460
Boris Prokhorov <deimos.75@mail.ru>
2024-04-19 21:30:07 +0300

    measure performance

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import { generateMeaningfulString, generateNumber, generatePerson, generateString, generateUUID } from 'test-data-utils'
+import { faker } from '@faker-js/faker'
+import { Benchmark } from 'data-generators-benchmark'
 
 
 for (let i = 0; i < 4097; i++) {
@@ -10,9 +12,29 @@ console.log(generateMeaningfulString({
 	separator: ' ',
 }))
 
-console.log(generatePerson())
+console.log(generatePerson({
+	gender: 'male',
+}))
 
+console.log(faker.person.firstName('male'))
 
-console.log(generateNumber())
+Benchmark.pushCandidate(generateUUID.bind(null, false), faker.string.uuid)
+Benchmark.plotAndSaveMeasurementTimesCharts()
+Benchmark.clearCandidates()
 
-console.log(generateString())
+Benchmark.pushCandidate(generateNumber.bind(null, {
+	min: 0,
+	max: 100
+}), faker.number.int.bind(null, {
+	min: 0,
+	max: 100
+}))
+Benchmark.plotAndSaveMeasurementTimesCharts()
+Benchmark.clearCandidates()
+
+Benchmark.pushCandidate(generateString.bind(null, {
+	charSet: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+	length: 1000
+}), faker.string.fromCharacters.bind(null, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 1000))
+Benchmark.plotAndSaveMeasurementTimesCharts()
+Benchmark.clearCandidates()
